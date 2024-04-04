bea30cfbcb84c5ee92faf3a3a62ff51caa587655
niko057 <nicatnamazov57.nn@gmail.com>
2024-04-04 10:54:18 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Partial_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("Nicat","niko57");
+            User user2 = new User("Pasha", "bmw");
+            User user3 = new User("Ali", "aliko");
+            user1.PasswordChecker("Abcd123kj");
+            user1.ShowInfo();
+            user2.PasswordChecker("Abcd123kj");
+            user2.ShowInfo();
+            user3.PasswordChecker("Abcd123kj");
+            user3.ShowInfo();
+        }
+
+       
+    }
+    
+
+}
---
+++ User.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_class
+{
+    public class User : IAccount
+    {
+
+        public string FullName;
+
+        public string Email;
+
+        public string Password;
+        public User(string fullname,string email)
+        {
+            FullName= fullname;
+            Email= email;   
+        }
+
+
+        public bool PasswordChecker(string password)
+        {
+           
+            bool isValidPassword = false;
+
+            do
+            {
+                Console.Write("Sifreni daxil edin: ");
+                password = Console.ReadLine();
+
+              
+                if (password.Length < 8)
+                {
+                    Console.WriteLine("Sifre min 8 simvoldan ibaret olmalidi.");
+                    continue;
+                }
+
+                bool hasUpperCase = false;
+                bool hasLowerCase = false;
+                bool hasDigit = false;
+
+                
+                foreach (char c in password)
+                {
+                    if (char.IsUpper(c))
+                    {
+                        hasUpperCase = true;
+                    }
+                    else if (char.IsLower(c))
+                    {
+                        hasLowerCase = true;
+                    }
+                    else if (char.IsDigit(c))
+                    {
+                        hasDigit = true;
+                    }
+                }
+
+               
+                if (!hasUpperCase)
+                {
+                    Console.WriteLine("Sifrede en azi 1 boyuk herf olmalidi.");
+                    continue;
+                }
+                if (!hasLowerCase)
+                {
+                    Console.WriteLine("Sifrede en azi 1 kicik herf olmalidi.");
+                    continue;
+                }
+                if (!hasDigit)
+                {
+                    Console.WriteLine("Sifrede en azi 1 reqem olmalidi.");
+                    continue;
+                }
+
+              
+                isValidPassword = true;
+
+            } while (!isValidPassword);
+
+            Console.WriteLine("Sifre Ugurla qebul olundu.");
+
+            return true;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("FullName:"+FullName);
+            Console.WriteLine("Email:"+Email);
+        }
+    }
+    interface IAccount
+    {
+        public bool PasswordChecker(string password);
+
+
+        public void ShowInfo();
+       
+    }
+}
